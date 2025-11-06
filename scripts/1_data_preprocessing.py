c752bf3fe30bb43e980798663404ec9c53210d3c
Ruben Prütz <rubenpruetz@gmail.com>
2025-11-06 15:04:14 +0100

    Updated preprocessing file to integrate preprocessing for REMIND-MAgPIE and to preprocess contextual information on suitability areas for land allocation

--- scripts/1_data_preprocessing.py
+++ scripts/1_data_preprocessing.py
@@ -10,34 +10,46 @@ import os
 import numpy.matlib
 from time import time
 from pathlib import Path
-
 from required_functions import *
 
 path_aim = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/aim_maps')
 path_gcam = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/gcam_maps')
 path_globiom = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/globiom_maps')
 path_image = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/image_maps')
+path_magpie = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/magpie_maps')
 path_uea = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/uea_maps/UEA_20km')
+path_ref_pot = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/reforest_potential')
+path_bioneng_pot = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity/Braun_et_al_2024_PB_BECCS/Results/1_source_data_figures/Fig2')
+path_all = Path('/Users/rpruetz/Documents/phd/primary/analyses/cdr_biodiversity')
 
 # load lookup table containing nc file information
-lookup_resample = pd.read_csv(path_uea / 'lookup_table_uea_resample_20km.csv')
-lookup_interpol = pd.read_csv(path_uea / 'lookup_table_uea_interpol_20km.csv')
+lookup_resample = pd.read_csv(
+    path_uea / 'lookup_table_uea_resample_20km.csv')
+
+lookup_interpol = pd.read_csv(
+    path_uea / 'lookup_table_uea_interpol_20km_2digits.csv')
+
+lookup_globiom_nc_df = pd.read_csv(path_globiom / 'lookup_table_globiom_nc_files.csv')
+lookup_globiom_nc_df['year'] = lookup_globiom_nc_df['year'].astype(str)
 
 lookup_aim_nc_df = pd.read_csv(path_aim / 'lookup_table_aim_nc_files.csv')
 
 lookup_gcam_nc_df = pd.read_csv(path_gcam / 'lookup_table_gcam_nc_files.csv')
-
-lookup_globiom_nc_df = pd.read_csv(path_globiom / 'lookup_table_globiom_nc_files.csv')
-lookup_globiom_nc_df['year'] = lookup_globiom_nc_df['year'].astype(str)
 
 lookup_image_nc_df = pd.read_csv(path_image / 'lookup_table_image_nc_files.csv')
 
 lookup_image_nc_pre = pd.read_csv(path_image /
                                   'lookup_table_image_nc_files_preprocessing.csv')
 
+lookup_magpie_nc_df = pd.read_csv(path_magpie / 'lookup_table_magpie_nc_files.csv')
+
+landfile_lookup = pd.read_csv(path_all / 'lookup_table_ar_bioenergy_files_all_models.csv')
+
 # specify project resolution
 target_res = (0.1666666666670000019, 0.1666666666670000019)  # uea resolution
-land_infos = np.array(['Afforestation', 'Bioenergy'])  # define for later
+land_infos = np.array(['Afforestation', 'Bioenergy', 'cropland_other',
+                       'forest_total', 'Cropland_total'])  # define for later
+land_infos_g = np.array(['Afforestation', 'Bioenergy'])  # define for later
 
 # %% adjust names of biodiv files
 for index, row in lookup_resample.iterrows():  # use lookup to resample uea files
@@ -52,49 +64,33 @@ for index, row in lookup_resample.iterrows():  # use lookup to resample uea file
         dst.write(tiff.astype(profile['dtype']))
 
 # linearily interpolate warming level between rasters
-inter_steps = 4  # number of desired interpolation steps
+inter_steps = 49  # number of desired interpolation steps
 
-for index, row in lookup_interpol.iterrows():  # use lookup to interpolate uea files
+for _, row in lookup_interpol.iterrows():  # use lookup table to define files
     lower_file = row['lower_file']
     upper_file = row['upper_file']
-    step_a = row['step_a']
-    step_b = row['step_b']
-    step_c = row['step_c']
-    step_d = row['step_d']
+    step_filenames = [row[f'step_{i}'] for i in range(1, inter_steps + 1)]
 
-    with rs.open(path_uea / lower_file, 'r') as input_tiff1:
-        lower_tiff = input_tiff1.read()
-        profile = input_tiff1.profile
+    with rs.open(path_uea / lower_file, 'r') as src_lower:
+        lower_tiff = src_lower.read()
+        profile = src_lower.profile
 
-    with rs.open(path_uea / upper_file, 'r') as input_tiff2:
-        upper_tiff = input_tiff2.read()
+    with rs.open(path_uea / upper_file, 'r') as src_upper:
+        upper_tiff = src_upper.read()
 
     tiff_diff = upper_tiff - lower_tiff
-    step_1 = tiff_diff * (1/(inter_steps+1)) + lower_tiff
-    step_2 = tiff_diff * (2/(inter_steps+1)) + lower_tiff
-    step_3 = tiff_diff * (3/(inter_steps+1)) + lower_tiff
-    step_4 = tiff_diff * (4/(inter_steps+1)) + lower_tiff
 
-    filenames = [step_a, step_b, step_c, step_d]
-    files = [step_1, step_2, step_3, step_4]
+    for i, filename in enumerate(step_filenames, start=1):
+        interpol_tiff = tiff_diff * (i/(inter_steps + 1)) + lower_tiff
 
-    for filename, file in zip(filenames, files):
         with rs.open(path_uea / filename, 'w', **profile) as dst:
-            dst.write(file.astype(profile['dtype']))
+            dst.write(interpol_tiff.astype(profile['dtype']))
 
 # create binary raster based on refugia threshold (0.75) using binary_converter
-input_files = ['bio1.0_near.tif', 'bio1.1_near.tif', 'bio1.2_near.tif',
-               'bio1.3_near.tif', 'bio1.4_near.tif', 'bio1.5_near.tif',
-               'bio1.6_near.tif', 'bio1.7_near.tif', 'bio1.8_near.tif',
-               'bio1.9_near.tif', 'bio2.0_near.tif', 'bio2.1_near.tif',
-               'bio2.2_near.tif', 'bio2.3_near.tif', 'bio2.4_near.tif',
-               'bio2.5_near.tif', 'bio2.6_near.tif', 'bio2.7_near.tif',
-               'bio2.8_near.tif', 'bio2.9_near.tif', 'bio3.0_near.tif',
-               'bio3.1_near.tif', 'bio3.2_near.tif', 'bio3.3_near.tif',
-               'bio3.4_near.tif', 'bio3.5_near.tif', 'bio3.6_near.tif',
-               'bio3.7_near.tif', 'bio3.8_near.tif', 'bio3.9_near.tif',
-               'bio4.0_near.tif', 'bio4.1_near.tif', 'bio4.2_near.tif',
-               'bio4.3_near.tif', 'bio4.4_near.tif', 'bio4.5_near.tif']
+input_files = []
+ts = [round(x, 2) for x in np.arange(1.0, 4.51, 0.01)]
+for t in ts:
+    input_files.append(f'bio{t}_near.tif')
 
 for input_file in input_files:
     output_file = input_file.replace('near.tif', 'bin.tif')
@@ -148,7 +144,7 @@ for scenario in scenarios:
                                                             masked=True)
         total_forest = sum(forest_dict.values())
         total_forest = total_forest * 0.01  # 0-100 --> 0-1
-        total_forest = total_forest.clip(max=1) # cap at 1
+        total_forest = total_forest.clip(max=1)  # cap at 1
         total_f_name = f'GCAM_forest_total_{scenario}_{year}.tif'
         total_forest.rio.to_raster(path_gcam / total_f_name, driver='GTiff')
 
@@ -191,7 +187,7 @@ land_area_calculation(path_gcam, 'bin_land.tif', 'GCAM_max_land_area_km2.tif')
 max_land_area = rioxarray.open_rasterio(path_gcam / 'GCAM_max_land_area_km2.tif',
                                         masked=True)
 
-for land_info in land_infos:  # calculate areas based on surface and land use fractions
+for land_info in land_infos_g:  # calculate areas based on surface and land use fractions
     for scenario in scenarios:
         for year in years:
 
@@ -229,33 +225,100 @@ for index, row in lookup_image_nc_pre.iterrows():
     nc_file_xr.coords['time_lc'] = (('time_lc'), np.arange(len(nc_file_xr.coords['time_lc'])))
     nc_file_xr.to_netcdf(path_image / output_name)
 
+# calculate maximum land area for IMAGE based on available area file
+nc_file_xr = xr.open_dataset(path_image / 'GAREACELLNOWATER_30MIN.NC')
+nc_file_xr = nc_file_xr[['longitude', 'latitude', 'time', 'GAREACELLNOWATER_30MIN']]
+nc_file_xr = nc_file_xr.transpose('time', 'latitude', 'longitude')
+nc_file_xr.to_netcdf(path_image / '_GAREACELLNOWATER_30MIN.NC')
+
+nc_file = rioxarray.open_rasterio(path_image / '_GAREACELLNOWATER_30MIN.NC',
+                                  decode_times=False,
+                                  band_as_variable=True)
+data_array_proj = nc_file.rio.write_crs('EPSG:4326')
+data_array_proj = data_array_proj['band_1']
+data_array_proj.rio.to_raster(path_image / '_GAREACELLNOWATER_30MIN.tif',
+                              driver='GTiff')
+
+with rs.open(path_image / '_GAREACELLNOWATER_30MIN.tif') as src:
+    data = src.read(1)
+    profile = src.profile.copy()
+    profile.update(count=1)
+with rs.open(path_image / '_GAREACELLNOWATER_30MIN.tif', 'w', **profile) as dst:
+    dst.write(data, 1)
+
+tiff_resampler(path_image / '_GAREACELLNOWATER_30MIN.tif', target_res, 'nearest',
+               path_image / '_GAREACELLNOWATER_30MIN.tif')
+
+original = 0.5  # original file resolution
+target = 0.1666666666670000019  # target resolution for analysis
+scaling_factor = (original / target)**2  # scaling factor for resampling
+
+max_area = rioxarray.open_rasterio(path_image / '_GAREACELLNOWATER_30MIN.tif', masked=True)
+max_area = max_area / scaling_factor
+max_area.rio.to_raster(path_image / 'IMAGE_max_land_area_km2.tif')
+
+# use same file for AIM area as this gives better aligned results with SSP Db
+max_area.rio.to_raster(path_aim / 'AIM_max_land_area_km2.tif')
+
+# calculate maximum land area for MAgPIE based on available area file
+nc_file = rioxarray.open_rasterio(path_magpie / 'grid_cell_land_area.nc',
+                                  decode_times=False,
+                                  band_as_variable=True)
+data_array_proj = nc_file.rio.write_crs('EPSG:4326')
+data_array_proj = data_array_proj['band_1']
+data_array_proj.rio.to_raster(path_magpie / 'grid_cell_land_area.tif',
+                              driver='GTiff')
+
+with rs.open(path_magpie / 'grid_cell_land_area.tif') as src:
+    data = src.read(1)
+    profile = src.profile.copy()
+    profile.update(count=1)
+with rs.open(path_magpie / 'grid_cell_land_area.tif', 'w', **profile) as dst:
+    dst.write(data, 1)
+
+# resample land use data to resolution of biodiv data
+tiff_resampler(path_magpie / 'grid_cell_land_area.tif', target_res, 'nearest',
+               path_magpie / 'grid_cell_land_area.tif')
+
+max_area = rioxarray.open_rasterio(path_magpie / 'grid_cell_land_area.tif', masked=True)
+max_area = (max_area / scaling_factor) * 10000  # adjust unit
+max_area.rio.to_raster(path_magpie / 'MAgPIE_max_land_area_km2.tif')
+
 # %% write crs, convert to tif, and create individual tifs per year and variable
+
 start = time()
 
-models = ['AIM', 'GLOBIOM', 'IMAGE']
+# unpack and preprocess AIM, GLOBIOM, IMAGE, and MAgPIE NC files
+models = ['AIM', 'GLOBIOM', 'IMAGE', 'MAgPIE']
 
 for model in models:
 
-    if model == 'AIM':
-        path = path_aim
-        lookup_table = lookup_aim_nc_df
-    elif model == 'GLOBIOM':
+    if model == 'GLOBIOM':
         path = path_globiom
         lookup_table = lookup_globiom_nc_df
+    elif model == 'AIM':
+        path = path_aim
+        lookup_table = lookup_aim_nc_df
     elif model == 'IMAGE':
         path = path_image
         lookup_table = lookup_image_nc_df
+    elif model == 'MAgPIE':
+        path = path_magpie
+        lookup_table = lookup_magpie_nc_df
 
-    for index, row in lookup_table.iterrows():  # use lookup to resample files
+    for index, row in lookup_table.iterrows():  # use lookup to resample uea files
         input_file = row['nc_file']
         band = row['band']
+        year = row['year']
         output_name = row['output_name']
 
         nc_file = rioxarray.open_rasterio(path / input_file,
                                           decode_times=False,
                                           band_as_variable=True)
         data_array_proj = nc_file.rio.write_crs('EPSG:4326')
-        data_array_proj = data_array_proj['band_' + str(band)]
+        data_array_proj = data_array_proj[band]
+        if model == 'MAgPIE':
+            data_array_proj = data_array_proj.sel(time=year)
         data_array_proj.rio.to_raster(path / 'temp_large_file.tif',
                                       driver='GTiff')
 
@@ -278,7 +341,7 @@ for model in models:
 
     for scenario in scenarios:
         for year in years:
-            # only relevant for IMAGE
+            # only relevant for IMAGE and MAgPIE
             try:
                 energy_crops_ir = f'{model}_energy_crops_ir_{scenario}_{year}.tif'
                 energy_crops_rf = f'{model}_energy_crops_rf_{scenario}_{year}.tif'
@@ -318,20 +381,20 @@ for model in models:
                 print(f'Error processing: {e}')
                 continue
 
-    # compute afforestation for all years vs base year
+    # compute afforestation for all years vs 2010
     for scenario in scenarios:
-        file_baseyr = f'{model}_forest_total_{scenario}_2010.tif'
+        file_2010 = f'{model}_forest_total_{scenario}_2010.tif'
 
         for year in years:
             forest_file = f'{model}_forest_total_{scenario}_{year}.tif'
             ar_file_yr = f'{model}_Afforestation_{scenario}_{year}.tif'
 
-            forest_base_yr = rioxarray.open_rasterio(
-                path / file_baseyr, masked=True)
+            forest_2010 = rioxarray.open_rasterio(
+                path / file_2010, masked=True)
             forest_yr = rioxarray.open_rasterio(
                 path / forest_file, masked=True)
 
-            forest_change = (forest_yr - forest_base_yr)  # -ve=loss; +ve=gain
+            forest_change = (forest_yr - forest_2010)  # -ve=loss; +ve=gain
 
             gain_yr = forest_change.where(
                 (forest_change > 0) | forest_change.isnull(), 0)
@@ -339,16 +402,15 @@ for model in models:
             gain_yr.rio.to_raster(path / ar_file_yr, driver='GTiff')
 
     # calculate grid area based on arbitrarily chosen input file
-    arbit_input = rioxarray.open_rasterio(
-        path / f'{model}_Afforestation_SSP1-19_2050.tif', masked=True)
+    if model not in ['IMAGE', 'MAgPIE', 'AIM']:  # for these models use predefined file
+        arbit_input = rioxarray.open_rasterio(
+            path / f'{model}_Afforestation_SSP1-19_2050.tif', masked=True)
+        bin_land = arbit_input.where(arbit_input.isnull(), 1)  # all=1 if not nodata
+        bin_land.rio.to_raster(path / 'bin_land.tif', driver='GTiff')
+        land_area_calculation(path, 'bin_land.tif', f'{model}_max_land_area_km2.tif')
 
-    bin_land = arbit_input.where(arbit_input.isnull(), 1)  # all=1 if not nodata
-    bin_land.rio.to_raster(path / 'bin_land.tif', driver='GTiff')
-
-    land_area_calculation(path, 'bin_land.tif', f'{model}_max_land_area_km2.tif')
-    max_land_area = rioxarray.open_rasterio(path /
-                                            f'{model}_max_land_area_km2.tif',
-                                            masked=True)
+    max_area = rioxarray.open_rasterio(path / f'{model}_max_land_area_km2.tif',
+                                       masked=True)
 
     # calculate land use areas based on total surface and land use fractions
     for land_info in land_infos:
@@ -360,8 +422,8 @@ for model in models:
                     land_fract = rioxarray.open_rasterio(
                         path / processing, masked=True)
 
-                    land_fract = land_fract.rio.reproject_match(max_land_area)
-                    land_area = land_fract * max_land_area
+                    land_fract = land_fract.rio.reproject_match(max_area)
+                    land_area = land_fract * max_area
 
                     land_area.rio.to_raster(path / processing,
                                             driver='GTiff')
@@ -371,3 +433,82 @@ for model in models:
 
 end = time()
 print(f'Runtime {(end - start) / 60} min')
+
+# %% process maps showing potential for reforestation and bioenergy cropland
+
+# create binary map for reforestation potential based on Fesenmyer et al. 2025
+with rs.open(path_ref_pot / 'for_ref_tco2eha.tif') as src:
+    data = src.read()
+    profile = src.profile
+
+ref_pot = np.where(data > 0, 1, np.where(data < 1, 0, data))
+profile.update(dtype=rs.float32)
+
+with rs.open(path_ref_pot / 'ref_suit.tif', 'w', **profile) as dst:
+    dst.write(ref_pot.astype(profile['dtype']))
+
+tiff_resampler(path_ref_pot / 'ref_suit.tif', target_res, 'nearest',
+               path_ref_pot / 'ref_suit.tif')
+
+# create binary map for area not suitable for reforestation
+with rs.open(path_ref_pot / 'ref_suit.tif') as src:
+    data = src.read()
+    profile = src.profile
+
+no_ref_pot = np.where(data == 1, 0, np.where(data == 0, 1, data))
+
+with rs.open(path_ref_pot / 'ref_not_suit.tif', 'w', **profile) as dst:
+    dst.write(no_ref_pot.astype(profile['dtype']))
+
+tiff_resampler(path_ref_pot / 'ref_not_suit.tif', target_res, 'nearest',
+               path_ref_pot / 'ref_not_suit.tif')
+
+# create binary map for bioenergy cropland constrained potential based on Braun et al. 2025
+# note: Braun et al. focus on BECCS but applicable to bioenergy cropland without CCS
+nc_file = rioxarray.open_rasterio(path_bioeng_pot / 'BECCS_area_fraction_PB-B.nc',
+                                  decode_times=False,
+                                  band_as_variable=True)
+data_array_proj = nc_file.rio.write_crs('EPSG:4326')
+data_array_proj.rio.to_raster(path_bioeng_pot / 'beccs_suit.tif', driver='GTiff')
+
+with rs.open(path_bioeng_pot / 'beccs_suit.tif') as src:
+    data = src.read()
+    profile = src.profile
+
+output_data = np.where((data > 0) & (data <= 1), 1, data)
+profile.update(dtype=rs.float32)
+
+with rs.open(path_bioeng_pot / 'beccs_suit.tif', 'w', **profile) as dst:
+    dst.write(output_data.astype(profile['dtype']))
+
+tiff_resampler(path_bioeng_pot / 'beccs_suit.tif', target_res, 'nearest',
+               path_bioeng_pot / 'beccs_suit.tif')
+
+# create binary map for bioenergy cropland maximum potential based on Braun et al. 2025
+nc_file = rioxarray.open_rasterio(path_bioeng_pot / 'BECCS_area_fraction_CDRonly.nc',
+                                  decode_times=False,
+                                  band_as_variable=True)
+data_array_proj = nc_file.rio.write_crs('EPSG:4326')
+data_array_proj.rio.to_raster(path_bioeng_pot / 'beccs_max_pot.tif', driver='GTiff')
+
+with rs.open(path_bioeng_pot / 'beccs_max_pot.tif') as src:
+    data = src.read()
+    profile = src.profile
+
+output_data = np.where((data > 0) & (data <= 1), 1, data)
+profile.update(dtype=rs.float32)
+
+with rs.open(path_bioeng_pot / 'beccs_max_pot.tif', 'w', **profile) as dst:
+    dst.write(output_data.astype(profile['dtype']))
+
+tiff_resampler(path_bioeng_pot / 'beccs_max_pot.tif', target_res, 'nearest',
+               path_bioeng_pot / 'beccs_max_pot.tif')
+
+# create binary map outside bioenergy cropland constrained potential (not suitable area)
+max_pot = rioxarray.open_rasterio(path_bioeng_pot / 'beccs_max_pot.tif', masked=True)
+cons_pot = rioxarray.open_rasterio(path_bioeng_pot / 'beccs_suit.tif', masked=True)
+not_suitable = max_pot - cons_pot
+
+# account for slight mismatch between max and constrained potential
+not_suitable = not_suitable.where(not_suitable == 1, 0)
+not_suitable.rio.to_raster(path_bioeng_pot / 'beccs_not_suit.tif', driver='GTiff')
