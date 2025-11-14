90abf0ee3a3284ad62ea9042dda0cfc67d20999e
Ruben Prütz <rubenpruetz@gmail.com>
2025-11-14 11:21:34 +0100

    Updated supplementary script to plot AR6 land use changes across models and scenarios

--- scripts/3_supplementary_analysis.py
+++ scripts/3_supplementary_analysis.py
@@ -31,14 +31,25 @@ ar6_db = pd.read_csv(path_ar6_data / 'AR6_Scenarios_Database_World_v1.1.csv')
 lookup_mi_luc_df = pd.read_csv(path_project / 'lookup_table_ar_bioenergy_files_all_models.csv')
 lookup_mi_luc_df['year'] = lookup_mi_luc_df['year'].astype(str)
 
-# %% get temperatures for SSP-RCP combinations
-all_years = [str(year) for year in range(2020, 2101)]
+# define lists
+years = ['2010', '2020', '2030', '2040', '2050', '2060', '2070', '2080',
+         '2090', '2100']
 
 models = ['MESSAGE-GLOBIOM 1.0', 'AIM/CGE 2.0', 'GCAM 4.2', 'IMAGE 3.0.1',
           'REMIND-MAgPIE 1.5']
 scenarios = ['SSP1-19', 'SSP1-26', 'SSP1-34', 'SSP1-45', 'SSP2-19', 'SSP2-26',
              'SSP2-34', 'SSP2-45', 'SSP3-34', 'SSP3-45']
 variable = ['AR6 climate diagnostics|Surface Temperature (GSAT)|MAGICCv7.5.3|50.0th Percentile']
+
+variables = ['Land Cover|Built-up Area', 'Land Cover|Cropland',
+             'Land Cover|Cropland|Energy Crops', 'Land Cover|Forest',
+             'Land Cover|Pasture']
+variables_adjust = ['Land Cover|Built-up Area', 'Other cropland',
+                    'Land Cover|Forest', 'Land Cover|Cropland|Energy Crops',
+                    'Land Cover|Pasture']
+
+# %% get temperatures for SSP-RCP combinations
+all_years = [str(year) for year in range(2020, 2101)]
 
 ar6_data = ar6_db.loc[ar6_db['Variable'].isin(variable)]
 ar6_data = ar6_data.loc[ar6_data['Model'].isin(models)]
@@ -182,3 +193,155 @@ for thres in thresholds:
     plt.title(f"Model agreement {file_scenario} {file_year} \n({recovery} & min. 'loss' of {thres}%)",
               fontsize=15, x=0.375, y=0.125, ha='left')
     plt.show()
+
+# %% get AR6 land cover data for SSP-RCP combinations
+ar6_db = ar6_db.loc[ar6_db['Model'].isin(models)]
+ar6_db = ar6_db.loc[ar6_db['Scenario'].isin(scenarios)]
+lc_data = ar6_db.loc[ar6_db['Variable'].isin(variables)]
+lc_data = lc_data[['Model', 'Scenario', 'Variable'] + years].copy()
+
+lc_data = pd.melt(lc_data, id_vars=['Model', 'Scenario', 'Variable'],
+                  value_vars=years, var_name='Year', value_name='Value')
+lc_ar6 = lc_data.copy()
+
+# distinguish between energy cropland and other cropland
+cropland = lc_data.loc[lc_data['Variable'].isin(['Land Cover|Cropland'])]
+cropland_energy = lc_data.loc[lc_data['Variable'].isin(['Land Cover|Cropland|Energy Crops'])]
+
+cropland_other = pd.merge(cropland, cropland_energy,
+                          on=['Model', 'Scenario', 'Year'],
+                          suffixes=['_all', '_energy'])
+
+cropland_other['Value'] = cropland_other['Value_all'] - cropland_other['Value_energy']
+cropland_other['Variable'] = 'Other cropland'
+cropland_other = cropland_other[['Model', 'Scenario', 'Variable', 'Year',
+                                 'Value']].copy()
+lc_data = pd.concat([lc_data, cropland_other], axis=0)
+
+# calculate change from base year (2010)
+lc_data = lc_data.loc[lc_data['Variable'].isin(variables_adjust)]
+lc_2010 = lc_data.query('Year == "2010"').reset_index(drop=True)
+lc_data = pd.merge(lc_data, lc_2010, on=['Model', 'Scenario', 'Variable'],
+                   suffixes=['', '_2010'])
+
+lc_data['Change'] = lc_data['Value'] - lc_data['Value_2010']
+lc_data['Change'] = lc_data['Change'] / 100  # from Mha to Mkm2
+
+# plot supplementary figure on land use changes based on AR6 Scenarios Database
+lc_data['SSP'] = lc_data['Scenario'].str.split('-').str[0]
+lc_data['RCP'] = lc_data['Scenario'].str.split('-').str[1]
+lc_data['Year'] = lc_data['Year'].astype(int)
+
+lc_data.replace({'Model': {'AIM/CGE 2.0': 'AIM',
+                           'MESSAGE-GLOBIOM 1.0': 'GLOBIOM',
+                           'IMAGE 3.0.1': 'IMAGE',
+                           'GCAM 4.2': 'GCAM',
+                           'REMIND-MAgPIE 1.5': 'MAgPIE'}}, inplace=True)
+
+lc_data.replace({'Variable': {'Land Cover|Built-up Area': 'Built-up',
+                              'Land Cover|Cropland|Energy Crops': 'Energy cropland',
+                              'Land Cover|Forest': 'Forest',
+                              'Land Cover|Pasture': 'Pasture'}}, inplace=True)
+
+var_pal = {'Built-up': 'dimgrey',
+           'Energy cropland': 'blue',
+           'Forest': 'forestgreen',
+           'Other cropland': 'brown',
+           'Pasture': 'gold'}
+all_vars = sorted(lc_data['Variable'].unique())
+#%%
+fig, axes = plt.subplots(5, 4, figsize=(9, 9), sharex=True, sharey=True)
+sns.lineplot(data=lc_data.query('Model == "AIM" & RCP == "19"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=True, ax=axes[0, 0])
+sns.lineplot(data=lc_data.query('Model == "GCAM" & RCP == "19"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[1, 0])
+sns.lineplot(data=lc_data.query('Model == "GLOBIOM" & RCP == "19"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[2, 0])
+sns.lineplot(data=lc_data.query('Model == "IMAGE" & RCP == "19"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[3, 0])
+sns.lineplot(data=lc_data.query('Model == "MAgPIE" & RCP == "19"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[4, 0])
+
+sns.lineplot(data=lc_data.query('Model == "AIM" & RCP == "26"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[0, 1])
+sns.lineplot(data=lc_data.query('Model == "GCAM" & RCP == "26"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[1, 1])
+sns.lineplot(data=lc_data.query('Model == "GLOBIOM" & RCP == "26"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[2, 1])
+sns.lineplot(data=lc_data.query('Model == "IMAGE" & RCP == "26"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[3, 1])
+sns.lineplot(data=lc_data.query('Model == "MAgPIE" & RCP == "26"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[4, 1])
+
+sns.lineplot(data=lc_data.query('Model == "AIM" & RCP == "34"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[0, 2])
+sns.lineplot(data=lc_data.query('Model == "GCAM" & RCP == "34"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[1, 2])
+sns.lineplot(data=lc_data.query('Model == "GLOBIOM" & RCP == "34"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[2, 2])
+sns.lineplot(data=lc_data.query('Model == "IMAGE" & RCP == "34"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[3, 2])
+sns.lineplot(data=lc_data.query('Model == "MAgPIE" & RCP == "34"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[4, 2])
+
+sns.lineplot(data=lc_data.query('Model == "AIM" & RCP == "45"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[0, 3])
+sns.lineplot(data=lc_data.query('Model == "GCAM" & RCP == "45"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[1, 3])
+sns.lineplot(data=lc_data.query('Model == "GLOBIOM" & RCP == "45"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[2, 3])
+sns.lineplot(data=lc_data.query('Model == "IMAGE" & RCP == "45"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[3, 3])
+sns.lineplot(data=lc_data.query('Model == "MAgPIE" & RCP == "45"'),
+             x='Year', y='Change', hue='Variable', palette=var_pal,
+             errorbar=('pi', 100), estimator='median', legend=False, ax=axes[4, 3])
+
+for ax in axes.flat:
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.set_xlim(2010, 2100)
+    ax.set_xticks([2010, 2055, 2100])
+    ax.tick_params(axis='x', labelsize=11)
+    ax.tick_params(axis='y', labelsize=11)
+    ax.grid(True, axis='y', linestyle='--', linewidth=0.5, alpha=0.8)
+
+axes[0, 0].set_ylabel('AIM', fontsize=11)
+axes[1, 0].set_ylabel('GCAM', fontsize=11)
+axes[2, 0].set_ylabel('GLOBIOM', fontsize=11)
+axes[3, 0].set_ylabel('IMAGE', fontsize=11)
+axes[4, 0].set_ylabel('REMIND-MAgPIE', fontsize=11)
+
+axes[0, 0].set_title('RCP1.9')
+axes[0, 1].set_title('RCP2.6')
+axes[0, 2].set_title('RCP3.4')
+axes[0, 3].set_title('RCP4.5')
+
+axes[0, 0].legend(bbox_to_anchor=(-0.05, 1.55), loc='upper left', ncols=5,
+                  columnspacing=1, handlelength=0.7, handletextpad=0.4, fontsize=11)
+
+fig.supylabel(f'Land cover change from 2010 [Mkm$^2$] (SSP1-SSP3 range as shading)',
+              x=0.03, va='center', ha='center')
+
+plt.subplots_adjust(hspace=0.1)
+plt.subplots_adjust(wspace=0.4)
+sns.despine()
+plt.show()
